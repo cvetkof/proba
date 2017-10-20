23bcca40e3a4ee241a37c004919d3b7fdb16d254
cvetkof <cvetkoffff15@mail.ru>
2017-10-20 08:59:33 +0300

    2

---
+++ test-csharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            var x = "y";
+
+            while (x != "n")
+                {
+
+                Console.WriteLine("Введите первое число:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число:");
+                b = Convert.ToInt32(Console.ReadLine());
+                if (a > b)
+                    Console.WriteLine("Первое число больше второго");
+                else if (a < b)
+                    Console.WriteLine("Второе число больше первого");
+                else
+                    Console.WriteLine("Числа равны");
+                Console.WriteLine("Продолжить? (y/n)");
+                x = Console.ReadLine();
+
+            }
+                
+
+        }
+    }
+}
